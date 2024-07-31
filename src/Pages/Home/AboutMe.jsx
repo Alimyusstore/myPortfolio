6565e1005c5f5d9e466a37c92e669633e667c16d
Alimyustore <yusuftajudeenajibola@gmail.com>
2024-07-31 22:00:15 +0100

    my portfolio

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,19 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about_me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          I specialize in web and mobile development across various frameworks and technologies. <br /> My expertise lies in developing responsive websites and applications, seamlessly translating conceptual designs into robust, production-ready solutions. <br /> I excel at architecting codebases with a strong emphasis on reusability, scalability, and maintainability. <br />This approach enables organizations to navigate
+complex software ecosystems efficiently and adapt to technological landscapes. I'm passionate about leveraging my technical skills and creative problem-solving abilities to deliver high-quality, user-centric applications that make a tangible impact.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
